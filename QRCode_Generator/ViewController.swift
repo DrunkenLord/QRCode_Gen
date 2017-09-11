7e5422f9b15dc6a786a36b8fd77ea7fd0c21bf79
Mehul Singhal <mehulbtp@gmail.com>
2017-09-12 01:55:16 +0530

    Initial Commit

---
+++ QRCode_Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCode_Generator
+//
+//  Created by Mehul  Singhal  on 12/09/17.
+//  Copyright © 2017 Mehul  Singhal . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
